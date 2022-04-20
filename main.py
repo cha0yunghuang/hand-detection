abb5ad089cadf51f5de64243cad58af2b1d8af6f
cha0yunghuang <eric@erysia.com>
2022-04-20 17:14:44 +0800

    add cam

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import cv2
+import mediapipe as mp
+import time
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+
+while True:
+    ret, frame = cap.read()
+
+    if ret:
+
+        cv2.imshow('Capture', frame)
+
+    if cv2.waitKey(20) == ord('q'):
+        break
