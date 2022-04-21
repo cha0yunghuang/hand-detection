9ca8184bec9a627b64285965cca341f50587184a
cha0yunghuang <eric@erysia.com>
2022-04-21 12:56:15 +0800

    add h/l landmark circle

--- main.py
+++ main.py
@@ -31,19 +31,14 @@ while True:
         imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         # processed result
         result = hands.process(imgRGB)
-        # hand landmarks coordinate
-        # print(result.multi_hand_landmarks)
 
         # get the height and width size of every frame
         imgHeight = frame.shape[0]
         imgWidth = frame.shape[1]
 
-
         # if landmarks coordinate detected
         if result.multi_hand_landmarks:
             
-
-
             # draw landmark
             for handLms in result.multi_hand_landmarks:
                 mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS, handLmsStyle, handConStyle)
@@ -54,21 +49,30 @@ while True:
                     # landmark position coordinate setting
                     xPosition = np.int(lm.x * imgWidth)
                     yPosition = np.int(lm.y * imgHeight)
-                    #print(id, xPosition, yPosition)
-                    # put id string next to the landmark
-                    cv2.putText(frame, str(id), (xPosition-30,yPosition+5), cv2.FONT_ITALIC, 0.3, (255,255,255), 1)
 
-                    # highlight the landmark circle 
-                    if id == 4:
-                        cv2.circle(frame, (xPosition, yPosition), 5, (0, 255, 0), cv2.FILLED)
+                    # put id string next to the landmark
+                    # cv2.putText(frame, str(id), (xPosition-30,yPosition+5), cv2.FONT_ITALIC, 0.3, (255,255,255), 1)
 
                     # coordinate of each id in a list
                     coor = []
                     coor.append(xPosition)
                     coor.append(yPosition)
                     location.append(coor)
-                print(location)
+                #print(location)
 
+            finger4 = location[4]
+            finger8 = location[8]
+            fingerDistance = math.sqrt(((finger4[0]-finger8[0])**2)+((finger4[1]-finger8[1])**2))
+            #print(int(fingerDistance))
+
+            # if distance <70, show h/l landmark circle and yellow circle
+            if fingerDistance < 70:
+                thumb = cv2.circle(frame, (finger4[0], finger4[1]), 8, (0, 255, 0), cv2.FILLED)
+                forefinger = cv2.circle(frame, (finger8[0], finger8[1]), 8, (0, 255, 0), cv2.FILLED)
+                # volumeLine = cv2.line(frame, (finger4[0],finger4[1]), (finger8[0],finger8[1]), (255,255,255), 1)
+                centerCir = cv2.circle(frame, (int((finger4[0]+finger8[0])/2),int((finger4[1]+finger8[1])/2)), 8, (0, 255, 255), cv2.FILLED)
+                
+            
         # FPS
         cTime = time.time()
         fps = 1 / (cTime - pTime)
